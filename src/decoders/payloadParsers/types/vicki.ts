0831059bba6b71f9e63848b5242ccf0b5b809302
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-09-04 15:28:43 +0300

    feat: add vicki and AQI sensor parsers

--- src/decoders/payloadParsers/types/vicki.ts
+++ src/decoders/payloadParsers/types/vicki.ts
@@ -1,4 +1,4 @@
-export type KeepAliveData = {
+export type VickiKeepAliveData = {
 	reason: number
 	targetTemperature: number
 	sensorTemperature: number | undefined
@@ -17,23 +17,4 @@ export type KeepAliveData = {
 	antiFreezeProtection: boolean
 	valveOpenness: number
 	targetTemperatureFloat?: number
-}
-
-export enum DeviceType {
-	Vicki = 'vicki',
-	HTSensor = 'ht_sensor',
-	TFlood = 't_flood',
-	AQISensor = 'aqi_sensor',
-	TValve = 't_valve',
-	CO2Sensor = 'co2_sensor',
-	Thermostat = 'thermostat',
-	MCButton = 'mc_button',
-	OpenCloseSensor = 'open_close_sensor',
-	DskDevice = 'dsk_device',
-	WirelessThermostat = 'wireless_thermostat',
-	CO2Display = 'co2_display',
-	FanCoilThermostat = 'fan_coil_thermostat',
-	CO2DisplayLite = 'co2_display_lite',
-	HTDisplayLite = 'ht_display_lite',
-	Relay16 = 'relay_16',
 }
