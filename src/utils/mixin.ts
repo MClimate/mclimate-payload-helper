ec6c93c6eb127d2261b94f17a0562dbfbffb62e9
Yasen Dimitrov <y_dimitrov@ymail.com>
2024-10-11 17:28:31 +0300

    fix: mixin function now correctly copies static methods

--- src/utils/mixin.ts
+++ src/utils/mixin.ts
@@ -1,15 +1,10 @@
 // NOTE: this is the "old way" of doing mixins, but it's still useful for some cases
 // ref for new way: https://www.typescriptlang.org/docs/handbook/mixins.html
-
 export function applyMixins(derivedCtor: any, baseCtors: any[]) {
 	baseCtors.forEach((baseCtor) => {
-		Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
-			if (name !== 'constructor') {
-				Object.defineProperty(
-					derivedCtor.prototype,
-					name,
-					Object.getOwnPropertyDescriptor(baseCtor.prototype, name) || Object.create(null),
-				)
+		Object.getOwnPropertyNames(baseCtor).forEach((name) => {
+			if (name !== 'prototype' && name !== 'name' && name !== 'constructor') {
+				Object.defineProperty(derivedCtor, name, Object.getOwnPropertyDescriptor(baseCtor, name) || Object.create(null))
 			}
 		})
 	})
